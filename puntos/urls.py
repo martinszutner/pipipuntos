2ebd864f2ec1a51ae5811b50cfd4c2f79109ca8f
juan Martin Fernandez-Szutner <martinszutner@gmail.com>
2024-08-23 00:07:21 -0300

    make chages

--- puntos/urls.py
+++ puntos/urls.py
@@ -1,3 +1,47 @@
+# # # # from django.urls import path
+# # # # from django.conf.urls import handler404, handler500
+# # # # from django.shortcuts import redirect
+# # # # from . import views
+
+# # # Definir las funciones que manejarán los errores
+# # # # def redirect_to_home(request, exception=None):
+    # # # # return redirect('index')  # Aquí 'index' es el nombre de tu vista de inicio
+
+# # # # def redirect_to_home_500(request):
+    # # # # return redirect('index')  # Aquí 'index' es el nombre de tu vista de inicio
+
+# # # Asignar las funciones a los manejadores de errores
+# # # # handler404 = redirect_to_home
+# # # # handler500 = redirect_to_home_500
+
+# # # # urlpatterns = [
+    # # # tus otras rutas aquí
+    # # # # path('', include('pipi.urls')),  # Asegúrate de que la ruta al home esté incluida
+# # # # ]
+
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+URL configuration for pipipuntos project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
 from django.urls import path
 from . import views
 
--- puntos/views.py
+++ puntos/views.py
@@ -5,6 +5,21 @@ import requests
 # URL del CSV publicado
 CSV_URL = 'https://docs.google.com/spreadsheets/d/1XQF9Q51Yth5IgFbB-7n5BweFDyFi9qwkUrcSqvSL4sI/pub?gid=0&single=true&output=csv'
 
+def index1(request):
+    if request.method == "POST":
+        dni = request.POST.get("dni")
+        # Lógica de búsqueda de puntos y nombre en la base de datos o Google Sheets
+        resultado = search_in_csv(dni)  # Supongamos que esta es tu función de búsqueda
+        if resultado:
+            nombre = resultado['nombre']
+            puntos = resultado['puntos']
+            context = {'nombre': nombre, 'puntos': puntos}
+        else:
+            context = {'error': 'No se encontraron resultados para el DNI ingresado.'}
+        return render(request, 'puntos/index.html', context)
+    return render(request, 'puntos/index.html')
+
+
 def index(request):
     if request.method == 'POST':
         dni = request.POST.get('dni')
@@ -19,9 +34,10 @@ def index(request):
         else:
             context = {'error': "DNI no encontrado."}
         
-        return render(request, 'puntos/result.html', context)
+        return render(request, 'puntos/index.html', context)
 
     return render(request, 'puntos/index.html')
+
 
 def search_in_csv(dni):
     response = requests.get(CSV_URL)
@@ -35,6 +51,5 @@ def search_in_csv(dni):
             return row
     return None
 
-def scan_qr_code(image):
-    # Lógica para escanear y decodificar el QR, que debería extraer el DNI del QR.
-    pass
+def scan_qr_code():
+    return None
